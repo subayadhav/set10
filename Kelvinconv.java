372d1a0100dea446066ca28abaf8f88494313a8c
subayadhav <suba10499@gmail.com>
2019-02-03 21:31:28 +0530

    Create Kelvinconv.java

---
+++ Kelvinconv.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+  public class Kelvinconv{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter Celcius value:");
+      int cel=sc.nextInt();
+      int kel=cel+273;
+      System.out.println("Kelvin:"+kel);
+      
+    }
+} 
