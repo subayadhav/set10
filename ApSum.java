b6f0054709372b3c5f86aa9025a7708ff617f2ab
subayadhav <suba10499@gmail.com>
2019-02-03 21:33:15 +0530

    Create ApSum.java

---
+++ ApSum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+  public class ApSum{
+    public static void main(String args[]){
+      Scanner s=new Scnner(System.in);
+      System.out.println("Enter A value:");
+      int a=s.nextInt();
+      System.out.println("Enter D value:");
+      int d=s.nextInt();
+      System.out.println("Enter N value:");
+      int n=s.nextInt();
+      System.out.println("AP:"+((a*a)+(d*d)+(n)));
+      }
+} 
